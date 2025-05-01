f64f46ec92e3dcf6290c556c5db423f0171beee3
jamuna-ramesh <jamuna26062002@gmail.com>
2025-05-01 13:25:16 +0530

    Done

--- src/main/lab.py
+++ src/main/lab.py
@@ -19,4 +19,11 @@
                 variable as the result.
 
 """
-
+def count_vowels(string):
+    vowels = "aeiouAEIOU"
+    count = 0
+    for char in string:
+        print(f"Checking: {char}")  
+        if char in vowels:
+            count += 1
+    return count
